f1e0fe9e676ec97ac8bc33994c6303fd73113c22
jony-sayfullaev <jonicks2000@gmail.com>
2022-11-16 11:35:58 -0500

    getting user contact information using database.csv file

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import csv
 
 app = Flask(__name__)
 
@@ -13,16 +14,30 @@ def html_page(page_name):
     return render_template(page_name)
 
 
-def write_to_file(data):
-    with open('database.txt', mode='a') as data_file:
-        data_file.write(f"{data}\n")
+# def write_to_file(data):
+#     with open('database.txt', mode='a') as data_file:
+#         data_file.write(f"{data}\n")
+
+def write_to_csv(data):
+    with open("database.csv", newline='', mode='a')as database_csv:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(database_csv, delimiter=',')
+        csv_writer.writerow(['email', 'subject', 'message'])
+        csv_writer.writerow([email, subject, message])
 
 
 @app.route("/submit_form", methods=['POST', 'GET'])
 def submit_form():
     if request.method == "POST":
-        data = request.form.to_dict()
-        write_to_file(data=data)
-        return redirect('/thankyou.html')
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data=data)
+            return redirect('/thankyou.html')
+        except:
+            return "did not save to database"
     else:
-        return "something went wrong :please try again"
+        return "something went wrong: please try again"
+
+
